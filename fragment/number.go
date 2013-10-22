37762f1b083404ad4190494ecc47ec4e2bcc0a9b
jfbus <jf@bustarret.com>
2013-10-22 17:17:27 +0200

    number fragments were broken

--- fragment/number.go
+++ fragment/number.go
@@ -8,10 +8,18 @@ import (
 type Number float64
 
 func (n *Number) Decode(enc interface{}) error {
-	dec, ok := enc.(Number)
+	dec, ok := enc.(float64)
 	if !ok {
 		return fmt.Errorf("unable to decode number fragment : %+v is a %s, not a number", enc, reflect.TypeOf(enc))
 	}
-	*n = dec
+	*n = Number(dec)
 	return nil
+}
+
+func (n *Number) AsText() string {
+	return fmt.Sprintf("%f", *n)
+}
+
+func (n *Number) AsHtml() string {
+	return fmt.Sprintf("<span class=\"number\">%f</span>", *n)
 }
