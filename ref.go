2782e3b096f507e8e05eae7f0cd80fb77e70b71f
Étienne Vallette d'Osia <dohzya@gmail.com>
2014-11-11 12:57:10 +0900

    ref: add Id field

--- ref.go
+++ ref.go
@@ -1,6 +1,7 @@
 package goprismic
 
 type Ref struct {
+	Id          string `json:"id"`
 	Ref         string `json:"ref"`
 	Label       string `json:"label"`
 	IsMasterRef bool   `json:"isMasterRef"`
